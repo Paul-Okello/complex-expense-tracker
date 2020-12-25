a4262732511a988e2ca33dcc0e8558d4761b20c8
Paul Okello <paulokello273@gmail.com>
2020-12-25 17:22:44 +0300

    ✅✅Action Creators ✅✅

--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,31 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import contextReducer from "./reducer";
 
+const initialState = [];
 export const ExpenseTarackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
+  const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+  //Actions
+  const deleteTransactions = (id) =>
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+
+  const addTransaction = (transaction) =>
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+
+  console.log(transactions);
+
   return (
-    <ExpenseTarackerContext.Provider value={{ appName: "Expense Tracker" }}>
+    <ExpenseTarackerContext.Provider
+      value={{ deleteTransactions, addTransaction }}
+    >
       {children}
     </ExpenseTarackerContext.Provider>
   );
