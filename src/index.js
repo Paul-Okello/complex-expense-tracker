76f4dc161d456395dbc16e237d1b3812d90256f7
Paul Okello <paulokello273@gmail.com>
2020-12-25 18:03:00 +0300

    🌰🌰Implements Provider🌰🌰

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "./context/context";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
