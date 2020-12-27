fe8454f8348e82fc1021f2ff88edf548ab4c0b34
Paul Okello <paulokello273@gmail.com>
2020-12-27 13:11:15 +0300

    🔁🔁Local Storage🔁🔁

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -6,10 +6,16 @@ const contextReducer = (state, action) => {
   switch (action.type) {
     case "DELETE_TRANSACTION":
       transactions = state.filter((t) => t.id !== action.payload);
+
+      //Local storage
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       return transactions;
 
     case "ADD_TRANSACTION":
       transactions = [action.payload, ...state];
+
+      //Local storage
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       return transactions;
 
     default:
